4dc6ba11caaeee5363e7cc9e9dee7ab6c7252e89
Mateusz Adamowicz <mateusz.adamowicz.1994@gmail.com>
2019-04-03 11:11:26 +0200

    3

---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+        public int PageSize = 4;
+
+        public ProductController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult List(int productPage = 1) 
+            => View(repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((productPage - 1)*PageSize)
+                .Take(PageSize));
+    }
+}
